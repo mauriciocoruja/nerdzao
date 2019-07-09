c1381a81cfafa90a5eed70a0e49fdb28f8c7e11e
Mauricio Barbosa <38763395+mauriciocoruja@users.noreply.github.com>
2019-07-09 19:57:32 -0300

    Update Main.java

--- ed/03-07-2019/02/Main.java
+++ ed/03-07-2019/02/Main.java
@@ -2,25 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-       int[] numurus = new int[101];
        String fizz = "Fizz";
        String buzz = "Buzz";
-
-       for (int i = 1; i <= 100; i++) {
-            numurus[i] = i;
-       }
-
-       for (int j = 1; j <= 100; j++) {
-           if(numurus[j] % 3 == 0 && numurus[j] % 5 == 0) {
-               System.out.println(fizz+buzz);
-           } else if (numurus[j] % 3 == 0) {
-               System.out.println(fizz);
-           } else if (numurus[j] % 5 == 0) {
-               System.out.println(buzz);
-           } else {
-               System.out.println(j);
-           }
-       }
+        
+       for (int i = 1; i <= 100 ; i++){
+            if (i % 3 == 0) {
+                System.out.println(fizz);
+            } else if (i % 5 == 0) {
+                System.out.println(buzz);
+            } else if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println(fizz+buzz);
+            } else{
+                System.out.println(i);
+            }
+        }
     }
 
 }
